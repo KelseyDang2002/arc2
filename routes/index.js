c8da50d45cc1979b451b5972207aeff1a1d07016
KelseyDang2002 <kelseydang813@gmail.com>
2025-01-17 15:46:14 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // template is index.jade
+  res.render('index', { title: 'Express', subtitle: 'Welcome user'});
+});
+
+// added a new route and changed the data without having to change the HTML page
+router.get('/tutorials', function(req, res, next) {
+  var courseName = req.query.course;
+  let courseTitle = 'Tutorials - ' + courseName;
+  // template is index.jade
+  res.render('index', { title: courseTitle, subtitle: 'Welcome ' + courseName + ' Dev'});
 });
 
 module.exports = router;
